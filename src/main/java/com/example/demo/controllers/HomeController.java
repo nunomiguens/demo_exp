b70fee7b52052d80996d90c111e83292a7475575
nunomiguens <nunomiguens@gmail.com>
2022-11-17 23:22:35 +0000

    Inicial commit

---
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class HomeController {
+    @GetMapping("/")
+    public String indexPage(Model model) {
+        model.addAttribute("title", "Spring Boot x Tailwind CSS");
+
+        return "home";
+    }
+
+}
